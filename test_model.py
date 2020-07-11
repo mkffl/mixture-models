c7de2ae7dc5820931cb9133adc5579a1cf897b6c
mkffl <m.kiffel@gmail.com>
2020-07-11 16:17:24 +0200

    format tests

--- test_model.py
+++ test_model.py
@@ -1,7 +1,21 @@
 import numpy as np
 from model import mixture_m_step_poisson, m_step_poisson, poisson_likelihood
 
-def test_mixture_m_step_poisson():
+@pytest.fixture(scope=module)
+def global_vars():
+    num_rows = 11
+
+    x1 = np.linspace(0,10,num_rows).reshape(num_rows,1)
+
+    x2 = np.linspace(10,20,num_rows).reshape(num_rows,1)
+
+    X = np.concatenate((x1, x2), axis=1)
+    return {
+        "num_rows": num_rows,
+        "X": X
+    }
+
+def test_mixture_m_step_poisson(global_vars):
     '''
     Test for the M step Poisson parameter.
 
@@ -10,85 +24,85 @@ def test_mixture_m_step_poisson():
     (equation 1.6). Set the weights to a constant
     and \lambda must be equal to the mean of X.
     This is the value assigned to lambda_expected.
-
-    constant is 0.5 for each component.
-    Mean of X is (5, 15).
-    So lambda must be [5, 15] for each component.
     '''
-    num_rows = 11
+    X = global_vars["X"]
+
+    X_mean = X.mean(axis=0)
 
     q_constant = 0.5
 
-    x1 = np.linspace(0,10,num_rows).reshape(num_rows,1)
+    # q is a two-dimensional array of 0.5
+    # i.e. each component distribution is a constant
+    q = np.ones(shape=(global_vars["num_rows"], 2)) * q_constant
 
-    x2 = np.linspace(10,20,num_rows).reshape(num_rows,1)
-
-    X = np.concatenate((x1, x2), axis=1)
-
-    q = np.ones((num_rows,2))*q_constant
-
-    result = mixture_m_step_poisson(X, q, q.shape[1], X.shape[1])
+    result = mixture_m_step_poisson(X=X, 
+                                    q=q, 
+                                    C=q.shape[1], 
+                                    D=X.shape[1])
 
     lambda_actual = result[0]
 
-    lambda_expected = np.array([X.mean(axis=0), X.mean(axis=0)])
+    # Mean of X is (5, 15) so lambda must be 
+    # [5, 15] for each component
+    lambda_expected = np.array([X_mean, 
+                                X_mean])
 
     assert np.alltrue(lambda_actual==lambda_expected)
 
-def test_m_step_poisson():
+def test_m_step_poisson(global_vars):
     '''
     Test for the prior probability.
 
-    \pi is the fraction of weighted x and
-    the number of observations (equ 1.7).
-    Set weights to a constant and \pi must
-    be equal to that constant.
+    \pi is the fraction of the sum of ones
+    weighted by q and the number of observations 
+    (equ 1.7). If weights q are a constant then 
+    \pi must be equal to that constant.
     
     Constant is (1/3, 2/3) for the components.
-    So \pi must be equal to (1/3, 2/3)
+    
 
     '''
-
-    num_rows = 11
-
+    # Constant is (1/3, 2/3) for each observation.
     constant = np.array([1/3,2/3])
+    
+    q = np.ones(shape=(global_vars["num_rows"], 2)) * constant
 
-    x1 = np.linspace(0,10,num_rows).reshape(num_rows,1)
+    # So \pi must be equal to (1/3, 2/3)
+    pi_expected = constant
 
-    x2 = np.linspace(10,20,num_rows).reshape(num_rows,1)
-
-    X = np.concatenate((x1, x2), axis=1)
-
-    q = np.ones((num_rows,2))*constant
-
-    result = m_step_poisson(X, q)
+    result = m_step_poisson(X=global_vars["X"], 
+                            q=q)
 
     pi_actual = result[0]
 
-    pi_expected = constant
-
     assert np.allclose(pi_actual, pi_expected)
 
-def test_poisson_likelihood():
+def test_poisson_likelihood(global_vars):
     '''
+    Test for the poisson likelihood function.
 
+    Based on the special case when \lambda is 1
+    i.e. one event per time period, and k=0 i.e. 
+    compute proba that no event happens.
     https://en.wikipedia.org/wiki/Poisson_distribution#Once_in_an_interval_events:_The_special_case_of_%CE%BB_=_1_and_k_=_0
     '''
-    num_rows = 11
-  
     proba_special_case = 1/np.exp(1)
 
-    X = np.zeros((num_rows, 2))
+    # Seek the probability of no event occuring
+    # i.e. x=0 hence use an array of zeros.
+    X = np.zeros(shape=(global_vars["num_rows"], 2))
 
-    lambda_special_case = np.array([1, 1])
+    lambda_test = np.array([1, 1])
 
     pi = np.array([0.5, 0.5])
 
-    mixture_params = (pi, lambda_special_case)
+    mixture_params = (pi, lambda_test)
 
-    pmf_actual = poisson_likelihood(0, mixture_params,X)
+    pmf_actual = poisson_likelihood(c=0, 
+                                    mixture_params=mixture_params,
+                                    X=X)
 
-    pmf_expected = np.ones((num_rows, 2))*proba_special_case
+    pmf_expected = np.ones(shape=(global_vars["num_rows"], 2)) * proba_special_case
 
     assert np.allclose(pmf_actual, pmf_expected)
 
